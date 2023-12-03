52edafb05506998d5cdf9d16372cdde092f00df2
abdul Razzaq <ar8937562@gmail.com>
2023-12-03 04:59:27 -0800

    added wrapper class to manage authentication and created services dir for managing auth services and other stuff

---
+++ lib/screens/Wrapper.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_firbase_tutorail/screens/Home/home.dart';
+
+class WrapperPage extends StatelessWidget {
+  const WrapperPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Home(
+
+    );
+  }
+}
